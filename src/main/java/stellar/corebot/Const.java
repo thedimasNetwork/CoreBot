64f2459b34b0561fa5699a9dc62812c98f25ee3e
thedimas3007 <dima.orischenko@gmail.com>
2023-08-27 21:52:15 +0200

    translate time formats

--- src/main/java/stellar/corebot/Const.java
+++ src/main/java/stellar/corebot/Const.java
@@ -7,9 +7,9 @@ public class Const {
     public static final String homeFolder = "bot/";
     public static final String[] tags = {"&lc&fb[D]&fr", "&lb&fb[I]&fr", "&ly&fb[W]&fr", "&lr&fb[E]", ""};
     public static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-    public static final String minuteFormat = "{0}m",
-            hourFormat = "{0}h {1}m",
-            dayFormat = "{0}d {1}h {2}m";
+    public static final String minuteFormat = "{0}м",
+            hourFormat = "{0}ч {1}м",
+            dayFormat = "{0}д {1}ч {2}м";
     public static final String statsFormat = """
             Блоков построено: {0}
             Блоков сломано: {1}
